2a9ebd4d403c4decc96e71b5730de5020a098aa6
Artem Ulko <ulkoart@gmail.com>
2019-10-08 16:41:15 +0300

    save

--- Views/ViewController.swift
+++ Views/ViewController.swift
@@ -10,11 +10,110 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var gameFieldView: GameFieldView!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var actionButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    func objectTappet() {
+        guard isGameActive else { return }
+        repositionImageWithTimer()
+        score += 1
+    }
+    
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        updateUI()
+    }
+    
+    @IBAction func actionButtonTapped(_ sender: UIButton) {
+        if isGameActive {
+            stopGame()
+        } else {
+            startGame()
+        }
+    }
+    
+    private var isGameActive = false
+    private var gameTimeLeft: TimeInterval = 0
+    private var gameTimer: Timer?
+    private var timer: Timer?
+    private let displayDuration: TimeInterval = 2
+    private var score = 0
+    
+    private func startGame() {
+        score = 0
+        repositionImageWithTimer()
+        gameTimer?.invalidate()
+        stepper.isEnabled = false
+        gameTimer = Timer.scheduledTimer(
+            timeInterval: 1,
+            target: self,
+            selector: #selector(gameTimerTick),
+            userInfo: nil,
+            repeats: true
+        )
+        gameTimeLeft = stepper.value
+        isGameActive = true
+        updateUI()
+    }
+    
+    private func stopGame() {
+        stepper.isEnabled = true
+        isGameActive = false
+        updateUI()
+        gameTimer?.invalidate()
+        timer?.invalidate()
+        scoreLabel.text = "Последний счет: \(score)"
+    }
+    
+    private func repositionImageWithTimer(){
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(
+            timeInterval: displayDuration,
+            target: self,
+            selector: #selector(moveImage),
+            userInfo: nil,
+            repeats: true
+        )
+        timer?.fire()
+    }
+    
+    private func updateUI(){
+        if isGameActive {
+            gameFieldView.isShapeHidden = !isGameActive
+            timeLabel.text = "Осталось: \(Int(gameTimeLeft)) сек"
+            actionButton.setTitle("Остановить", for: .normal)
+        } else {
+            timeLabel.text = "Время: \(Int(stepper.value)) сек"
+            actionButton.setTitle("Начать", for: .normal)
+        }
+    }
+    
+    
+    @objc private func gameTimerTick() {
+        gameTimeLeft -= 1
+        if gameTimeLeft <= 0 {
+            stopGame()
+        } else {
+            updateUI()
+        }
+
+    }
+    
+    @objc private func moveImage() {
+        gameFieldView.randomizeShapes()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        gameFieldView.layer.borderWidth = 1
+        gameFieldView.layer.borderColor = UIColor.gray.cgColor
+        gameFieldView.layer.cornerRadius = 5
+        updateUI()
+        gameFieldView.shapeHitHandler = { [weak self] in
+            self?.objectTappet()
+        }
     }
-
-
 }
 
